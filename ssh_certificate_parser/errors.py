d854cf10032e6a819c326e69a397e361a52a082f
James Brown <jbrown@easypost.com>
2021-01-04 16:11:39 -0800

    add github action and clean up some warnings

--- ssh_certificate_parser/errors.py
+++ ssh_certificate_parser/errors.py
@@ -5,13 +5,13 @@ class SSHCertificateParserError(Exception):
     pass
 
 
-@attr.s(frozen=True, hash=True, cmp=True)
+@attr.s(frozen=True, hash=True, eq=True, order=True)
 class UnsupportedKeyTypeError(SSHCertificateParserError):
     """This key has a type which we do not know how to parse"""
     key_type = attr.ib()
 
 
-@attr.s(frozen=True, cmp=True, hash=True)
+@attr.s(frozen=True, hash=True, eq=True, order=True)
 class UnsupportedCertificateTypeError(SSHCertificateParserError):
     """This key was signed with an unknown certificate algorithm"""
     cert_type = attr.ib()
